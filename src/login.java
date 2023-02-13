87d47805c5e36b11eded8281490e02331172d14b
Ernest <benitezernest33@gmail.com>
2023-02-13 19:38:36 +0800

    File Upload

---
+++ src/login.java
@@ -0,0 +1,106 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.SwingConstants;
+import java.awt.Insets;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Toolkit;
+
+public class login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtEmail;
+	private JLabel lblNewLabel_2;
+	private JPasswordField passwordField;
+	private JLabel lblNewLabel_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					login frame = new login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public login() {
+		setTitle("Roblox");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Ernest\\eclipse-workspace\\SecondSemester\\img\\icons8-roblox-48.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 244, 457);
+		contentPane = new JPanel();
+		contentPane.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		contentPane.setForeground(new Color(255, 255, 255));
+		contentPane.setBackground(new Color(23, 25, 30));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Sign in to");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 24));
+		lblNewLabel.setBounds(23, 132, 110, 67);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Roblox");
+		lblNewLabel_1.setForeground(new Color(255, 0, 0));
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.BOLD, 24));
+		lblNewLabel_1.setBounds(23, 170, 126, 39);
+		contentPane.add(lblNewLabel_1);
+		
+		txtEmail = new JTextField();
+		txtEmail.setMargin(new Insets(2, 4, 2, 2));
+		txtEmail.setText("Email");
+		txtEmail.setForeground(new Color(192, 192, 192));
+		txtEmail.setBounds(23, 210, 147, 20);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Ernest\\eclipse-workspace\\SecondSemester\\img\\icons8-roblox-48.png"));
+		lblNewLabel_2.setBounds(66, 83, 63, 65);
+		contentPane.add(lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnNewButton.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setBackground(new Color(255, 0, 0));
+		btnNewButton.setBorder(null);
+		btnNewButton.setBounds(81, 297, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(23, 254, 147, 20);
+		contentPane.add(passwordField);
+		
+		lblNewLabel_3 = new JLabel("© All rights reserved. Roblox, 2023.");
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		lblNewLabel_3.setBounds(23, 381, 189, 14);
+		contentPane.add(lblNewLabel_3);
+		
+	}
+}
